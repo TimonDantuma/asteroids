2c06a4fe9ceaa03595d08bd9b074a4f09e840ea9
TimonDantuma <timondantuma@gmail.com>
2025-05-16 14:27:44 +0200

    B: it was time

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+
+    pygame_clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    new_player = Player(x, y)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        new_player.draw(screen)
         pygame.display.flip()
+        pygame_clock.tick(60)
+        dt = pygame_clock.tick(60) / 1000
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
